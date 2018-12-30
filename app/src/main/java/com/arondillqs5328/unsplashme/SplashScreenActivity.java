af1cbaa5e6013b53b49901a91b8b56b2165ebc92
StepanRevytskyi <revytskyi.stepan@uzhnu.edu.ua>
2018-12-30 14:12:36 +0200

    migrate to AndroidX

--- app/src/main/java/com/arondillqs5328/unsplashme/SplashScreenActivity.java
+++ app/src/main/java/com/arondillqs5328/unsplashme/SplashScreenActivity.java
@@ -1,6 +1,6 @@
 package com.arondillqs5328.unsplashme;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
