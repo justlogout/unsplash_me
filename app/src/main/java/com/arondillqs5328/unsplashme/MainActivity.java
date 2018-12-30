7f401ee53430d78ca8d49ccbee68d4de163fca51
StepanRevytskyi <revytskyi.stepan@uzhnu.edu.ua>
2018-12-30 15:17:43 +0200

    set up SplashScreen

---
+++ app/src/main/java/com/arondillqs5328/unsplashme/MainActivity.java
@@ -0,0 +1,14 @@
+package com.arondillqs5328.unsplashme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/arondillqs5328/unsplashme/SplashScreenActivity.java
+++ app/src/main/java/com/arondillqs5328/unsplashme/SplashScreenActivity.java
@@ -1,13 +1,18 @@
 package com.arondillqs5328.unsplashme;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
