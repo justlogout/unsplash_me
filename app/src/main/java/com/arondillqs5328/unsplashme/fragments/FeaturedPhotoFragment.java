b07599f503d1b04b98c220c78651f526a220a298
StepanRevytskyi <revytskyi.stepan@uzhnu.edu.ua>
2019-01-01 16:29:44 +0200

    set up menu in all fragment refactor

--- app/src/main/java/com/arondillqs5328/unsplashme/fragments/FeaturedPhotoFragment.java
+++ app/src/main/java/com/arondillqs5328/unsplashme/fragments/FeaturedPhotoFragment.java
@@ -2,8 +2,12 @@ package com.arondillqs5328.unsplashme.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.arondillqs5328.unsplashme.R;
 
@@ -18,6 +22,12 @@ public class FeaturedPhotoFragment extends Fragment {
         return fragment;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,5 +35,27 @@ public class FeaturedPhotoFragment extends Fragment {
         setRetainInstance(true);
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.featured_photo_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.featured_latest:
+                Toast.makeText(getContext(), "featured latest", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.featured_oldest:
+                Toast.makeText(getContext(), "featured oldest", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.featured_popular:
+                Toast.makeText(getContext(), "featured popular", Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/arondillqs5328/unsplashme/fragments/NewPhotoFragment.java
+++ app/src/main/java/com/arondillqs5328/unsplashme/fragments/NewPhotoFragment.java
@@ -2,8 +2,12 @@ package com.arondillqs5328.unsplashme.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.arondillqs5328.unsplashme.R;
 
@@ -18,6 +22,12 @@ public class NewPhotoFragment extends Fragment {
         return fragment;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,5 +35,27 @@ public class NewPhotoFragment extends Fragment {
         setRetainInstance(true);
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.new_photo_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_latest:
+                Toast.makeText(getContext(), "new latest", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.new_oldest:
+                Toast.makeText(getContext(), "new oldest", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.new_popular:
+                Toast.makeText(getContext(), "new popular", Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
